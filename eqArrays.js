32d8c2a917728710ce159cb2474ed1ec0734bc91
Yzma <10090913+Yzma@users.noreply.github.com>
2023-01-10 14:08:56 -0500

    Implement eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+
+const assertEquals = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`)
+  } else {
+    console.log(`🛑Assertion Failed: [${actual}] !== [${expected}]`)
+  }
+}
+
+const eqArrays = function(first, second) {
+  if (first.length !== second.length) {
+    return false
+  }
+
+  for (let i = 0; i < first.length; i++) {
+    if (first[i] !== second[i]) {
+      return false
+    }
+  }
+  return true
+}
+
+assertEquals(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+assertEquals(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
+
+assertEquals(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+assertEquals(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
