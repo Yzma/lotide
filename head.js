6a8c801084db6b9ce75b39305ae44edffc7270f6
Yzma <10090913+Yzma@users.noreply.github.com>
2023-01-10 14:19:42 -0500

    Format head.js

--- head.js
+++ head.js
@@ -11,12 +11,12 @@ const assertEqual = function(actual, expected) {
 
 const head = function(array) {
 
-  if(!array) {
+  if (!array) {
     return null
   }
 
   return array[0]
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5,6,7]), 5)
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello")
