114b430dc321a7e2eb589c5624a3e0bc0eb7716a
Yzma <10090913+Yzma@users.noreply.github.com>
2023-01-12 14:34:28 -0500

    Implemenet takeUntil

---
+++ takeUntil.js
@@ -0,0 +1,16 @@
+
+const takeUntil = function(array, callback) {
+  let result = []
+  for (let item of array) {
+    if (callback(item)) {
+      break
+    } else {
+      result.push(item)
+    }
+  }
+  return result
+}
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5]
+const results1 = takeUntil(data1, x => x < 0)
+console.log(results1)
