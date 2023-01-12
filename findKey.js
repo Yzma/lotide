ce7cb75885098c6ae5f9f2e800b5c67979d50453
Yzma <10090913+Yzma@users.noreply.github.com>
2023-01-12 14:35:14 -0500

    Implemenet findKey

---
+++ findKey.js
@@ -0,0 +1,38 @@
+const assertEquals = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`)
+  } else {
+    console.log(`🛑Assertion Failed: [${actual}] !== [${expected}]`)
+  }
+}
+
+const findKey = function(key, callback) {
+  for (let i in key) {
+    if (callback(key[i])) {
+      return i
+    }
+  }
+  return null
+}
+
+const testObj = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}
+
+const key = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+assertEquals(findKey(testObj, x => x.stars === 1), "Blue Hill")
+assertEquals(findKey(testObj, x => x.stars === 2), "noma")
+assertEquals(findKey(testObj, x => x.stars === 3), "Akaleri")
