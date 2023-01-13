3b51ff45e6d7e20b838f3ae4b6e9d397fc013442
Yzma <10090913+Yzma@users.noreply.github.com>
2023-01-13 13:31:53 -0500

    Make eqArrays recursive

--- eqArrays.js
+++ eqArrays.js
@@ -17,7 +17,11 @@ const eqArrays = function(first, second) {
   }
 
   for (let i = 0; i < first.length; i++) {
-    if (first[i] !== second[i]) {
+    if (Array.isArray(first[i]) && Array.isArray(second[i])) {
+      if (!eqArrays(first[i], second[i])) {
+        return false
+      }
+    } else if (first[i] !== second[i]) {
       return false
     }
   }
@@ -29,3 +33,11 @@ assertEquals(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
 
 assertEquals(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
 assertEquals(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
+
+console.log('--- re ---')
+
+assertEquals(eqArrays([[2, 3], [3, 2]], [[2, 3], [3, 2]]), true) // true
+assertEquals(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true) // => true
+
+assertEquals(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false) // => false
+assertEquals(eqArrays([[2, 3], [4]], [[2, 3], 4]), false) // => false
