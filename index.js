6ce81a2fa75d5bd1ba6a6725ec94dee9700f919c
Yzma <10090913+Yzma@users.noreply.github.com>
2023-01-16 11:30:41 -0500

    Small format in index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
-const head   = require('./head')
-const tail   = require('./tail')
+const head = require('./head')
+const tail = require('./tail')
 const middle = require('./middle')
 
 module.exports = {
