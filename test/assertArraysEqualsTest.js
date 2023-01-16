f91ba5931e74663a5c3536dc076f1dc161e694a4
Yzma <10090913+Yzma@users.noreply.github.com>
2023-01-16 12:25:54 -0500

    Remove unused test files

--- test/assertArraysEqualsTest.js
+++
@@ -1,4 +0,0 @@
-// const assertArrayEquals = require('../assertArrayEquals')
-
-// assertArrayEquals([1, 2, 3], [1, 2, 3], true) // => true
-// assertArrayEquals([1, 2, 3], [3, 2, 1], false) // => false
--- test/assertEqualTest.js
+++
@@ -1,9 +0,0 @@
-// const assertEqual = require('../assertEqual')
-
-// assertEqual("Lighthouse Labs", "Bootcamp")
-// assertEqual(5, "5")
-// assertEqual("Same", "Same ") // Expected has a space at the end - should not pass
-
-// assertEqual("Same String", "Same String")
-// assertEqual("5", "5")
-// assertEqual(1, 1)
