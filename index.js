bedc38db2d0d91b7ea7c1004229affb170562afb
Yzma <10090913+Yzma@users.noreply.github.com>
2023-01-16 12:51:43 -0500

    Update index file to include all methods

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 
+const assertArrayEquals = require('./assertArrayEquals')
+const assertEqual = require('./assertEqual')
+const assertObjectEqual = require('./assertObjectEquals')
+const countLetters = require('./countLetters')
+const countOnly = require('./countOnly')
+const eqObjects = require('./eqObjects')
+const eqArrays = require('./eqArrays')
+const findKey = require('./findKey')
+const findKeyByValue = require('./findKeyByValue')
+const flatten = require('./flatten')
 const head = require('./head')
-const tail = require('./tail')
+const letterPositions = require('./letterPositions')
+const map = require('./map')
 const middle = require('./middle')
+const tail = require('./tail')
+const takeUntil = require('./takeUntil')
+const without = require('./without')
 
 module.exports = {
+  assertArrayEquals,
+  assertEqual,
+  assertObjectEqual,
+  countLetters,
+  countOnly,
+  eqObjects,
+  eqArrays,
+  findKey,
+  findKeyByValue,
+  flatten,
   head,
-  tail,
+  letterPositions,
+  map,
   middle,
+  tail,
+  takeUntil,
+  without
 }
